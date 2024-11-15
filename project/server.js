7f4c4544f2ef602c56eef6bca8ebc469509c0778
Will <williamraymondhopkins@gmail.com>
2024-11-15 16:10:29 +0800

    Update server.js to use ES modules

--- project/server.js
+++ project/server.js
@@ -1,13 +1,18 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
 
 // Serve static files from the React build directory
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(join(__dirname, 'dist')));
 
 // Handle all other routes by serving the index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(join(__dirname, 'dist', 'index.html'));
 });
 
 const PORT = process.env.SERVER_PORT || 3000;
